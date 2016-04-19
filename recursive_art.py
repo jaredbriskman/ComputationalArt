224f8cc4a7e9f95b003d8a3c2c00e702b786ea6f
jaredbriskman <jared.briskman@students.olin.edu>
2016-04-18 22:37:35 -0400

    cleaned up version, now with numpy execution

--- recursive_art.py
+++ recursive_art.py
@@ -3,14 +3,7 @@
 import random
 from PIL import Image
 import math
-
-
-#Functions
-two_param = [lambda a,b: a*b , lambda a,b: 0.5*(a+b)]
-one_param  = [lambda a: math.cos(math.pi*a), lambda a: math.sin(math.pi*a),
-lambda a: a**2, lambda a: a**3]
-end_param  = [lambda a,b: a, lambda a,b: b]
-functionList = one_param + two_param + end_param
+import numpy
 
 def build_random_function(min_depth, max_depth):
     """ Builds a random function of depth at least min_depth and depth
@@ -24,19 +17,29 @@ def build_random_function(min_depth, max_depth):
                  these functions)
     """
 
+    #Functions
+    funcs = {
+    "two_param":(lambda a,b: a*b , lambda a,b: 0.5*(a+b)),
+    "one_param":(lambda a: numpy.cos(math.pi*a), lambda a: numpy.sin(math.pi*a),
+    lambda a: a**2, lambda a: a**3),
+    "end_param":(lambda a,b: a, lambda a,b: b)
+    }
+    functionTuple = funcs["one_param"] + funcs["two_param"] + funcs["end_param"]
+
+    #Build
     depth = random.randint(min_depth, max_depth)
-    if depth == 1:
-        random_index = random.randint(-2,-1)
-        return functionList[random_index]
+    if depth == 1: #Base case
+        random_index = random.randint(0, len(funcs["end_param"]) -1)
+        return funcs["end_param"][random_index] #Choses between end functions
     else:
-        random_index = random.randint(0,len(functionList)-len(end_param))
-        if random_index > len(one_param) -1:
+        random_index = random.randint(0,len(functionTuple)-len(funcs["end_param"])) #Chose from non end functions
+        if random_index > len(funcs["one_param"]) -1:
             func1 = build_random_function(depth-1,depth-1)
             func2 = build_random_function(depth-1,depth-1)
-            return lambda x,y: functionList[random_index](func1(x,y),func2(x,y))
-        else:
+            return lambda x,y: functionTuple[random_index](func1(x,y),func2(x,y))
+        else: #one_param
             func1 = build_random_function(depth-1,depth-1)
-            return lambda x,y: functionList[random_index](func1(x,y))
+            return lambda x,y: functionTuple[random_index](func1(x,y))
 
 def remap_interval(val,
                    input_interval_start,
@@ -84,30 +87,8 @@ def color_map(val):
         >>> color_map(0.5)
         191
     """
-    # NOTE: This relies on remap_interval, which you must provide
     color_code = remap_interval(val, -1, 1, 0, 255)
-    return int(color_code)
-
-
-def test_image(filename, x_size=350, y_size=350):
-    """ Generate test image with random pixels and save as an image file.
-
-        filename: string filename for image (should be .png)
-        x_size, y_size: optional args to set image dimensions (default: 350)
-    """
-    # Create image and loop over all pixels
-    im = Image.new("RGB", (x_size, y_size))
-    pixels = im.load()
-    for i in range(x_size):
-        for j in range(y_size):
-            x = remap_interval(i, 0, x_size, -1, 1)
-            y = remap_interval(j, 0, y_size, -1, 1)
-            pixels[i, j] = (random.randint(0, 255),  # Red channel
-                            random.randint(0, 255),  # Green channel
-                            random.randint(0, 255))  # Blue channel
-
-    im.save(filename)
-
+    return color_code.astype('uint8') #For scalar
 
 def generate_art(filename, x_size=350, y_size=350):
     """ Generate computational art and save as an image file.
@@ -116,26 +97,16 @@ def generate_art(filename, x_size=350, y_size=350):
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
     # Functions for red, green, and blue channels - where the magic happens!
-
     red_function = build_random_function(7, 9)
     green_function = build_random_function(7, 9)
     blue_function = build_random_function(7, 9)
-    #red_function = ["x"]
-    #green_function = ["y"]
-    #blue_function = ["x"]
     # Create image and loop over all pixels
-    im = Image.new("RGB", (x_size, y_size))
-    pixels = im.load()
-    for i in range(x_size):
-        for j in range(y_size):
-            x = remap_interval(i, 0, x_size, -1, 1)
-            y = remap_interval(j, 0, y_size, -1, 1)
-            pixels[i, j] = (
-                    color_map(red_function(x, y)),
-                    color_map(green_function(x, y)),
-                    color_map(blue_function(x, y))
-                    )
-
+    x = numpy.tile(numpy.linspace(-1, 1, x_size), (y_size, 1)) #for X
+    y = numpy.tile(numpy.linspace(-1, 1, y_size), (x_size, 1)).T #for Y
+    r = red_function(x, y)
+    g = green_function(x, y)
+    b = blue_function(x, y)
+    im = Image.fromarray(color_map(numpy.stack((r,g,b), axis=2)), "RGB") #Stacking all three channels into an image
     im.save(filename)
 
 
